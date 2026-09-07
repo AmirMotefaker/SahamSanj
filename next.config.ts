af3d51446304d1474449b5d199497783705257c3
Amir Motefaker <amotef@gmail.com>
2026-09-07 18:54:03 +0330

    ci: publish development preview to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/SahamSanj",
+  assetPrefix: "/SahamSanj/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
